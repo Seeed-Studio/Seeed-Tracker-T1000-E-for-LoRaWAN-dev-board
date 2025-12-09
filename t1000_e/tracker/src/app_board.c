f3ad9d433e1decb176c9c529c5a8b19ac5d7c624
WayenWeng <jinyuan.weng@seeed.cc>
2025-12-09 09:49:02 +0800

    Update version to v1.2

--- t1000_e/tracker/src/app_board.c
+++ t1000_e/tracker/src/app_board.c
@@ -2,7 +2,7 @@
 #include "app_board.h"
 BoardVersion_t BoardVersion = {
     .Fields.SwMajor = 1,
-    .Fields.SwMinor = 1,
+    .Fields.SwMinor = 2,
 
     .Fields.HwMajor = 1,
     .Fields.HwMinor = 1,
